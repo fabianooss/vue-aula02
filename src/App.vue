491629bf77b1ff5b5f1daa6144a25c49c6179b9e
fabiano <fabiano.oss@gmail.com>
2019-09-16 21:50:43 -0300

    Computed, watch, ciclo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Campos calculados </h1>
+    <p> {{ nomeCompleto }} </p>
+    <input type="text" v-model="nome"/>
+
+    <br/>
+    <input type="text" 
+        v-model="filtro"
+        placeholder="Digite um nome para pesquisar"
+    />
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Nome</th>
+      </tr>
+      <tr v-for="c in carrosFiltrados" :key="c.id">
+        <td>{{c.id}}</td>
+        <td>{{c.nome}}</td>
+      </tr>
+    </table>
+
+    <h1>Observadores</h1>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nome : 'Fabiano',
+      sobrenome: 'Oss',
+      filtro: '',
+      carros: [
+        { nome: 'Lamborguini', id: 1},
+        { nome: 'Fusca', id: 2},
+        { nome: 'R-34', id: 3},
+        { nome: 'Chevete', id: 4},
+        { nome: 'Monza', id: 5},
+      ]
+    }
+  },
+  beforeMount() {
+    /* eslint-disable */
+    console.log('antes de montar os componentes')
+  },
+  mounted() {
+    /* eslint-disable */
+    console.log('mountou os componentes')
+  },
+  updated() {
+    /* eslint-disable */
+    console.log('atualizou os componentes')
+ç  },
+  watch: {
+    nome: function (val) {
+      /* eslint-disable */
+      console.log('Modificou o nome ' + val)
+    },
+    carrosFiltrados: function(val) {
+      /* eslint-disable */
+      console.log('Modificou o array ' + val)      
+    }
+  },
+  computed: {
+    nomeCompleto: function() {
+      return this.nome + " " + this.sobrenome
+    },
+    carrosFiltrados: function() {
+      if (this.filtro == '') {
+        return this.carros
+      }
+      return this.carros.filter(c => {
+        return c.nome.startsWith(this.filtro)
+      })
+    }
   }
 }
 </script>
